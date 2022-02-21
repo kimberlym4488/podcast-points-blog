b3b9f5832a98593351bbecd00dac0efece447031
SamusMoran <kimberlym4488@comcast.net>
2022-02-21 10:41:21 -0800

    Toast gradient updated successfully

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,9 @@ const userRoutes = require("./userRoutes");
 const reviewRoutes = require("./reviewRoutes");
 // const podcastRoutes = require("./podcastRoutes")
 
-
-
 //sets user login route to /api/user
 router.use("/user", userRoutes);
-router.use('/review', reviewRoutes)
+router.use("/review", reviewRoutes);
 // router.use('/podcast', podcastRoutes)
 //router.use("/html", htmlRoutes);
 
--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -2,6 +2,25 @@ const router = require("express").Router();
 const { withAuthJson } = require("../../utils/auth");
 const { Review, Podcast, User } = require("../../models");
 
+//get one review
+router.get("/:id", withAuthJson, async (req, res) => {
+  try {
+    const reviewData = await Review.findByPk({
+      ...req.body,
+      user_id: req.session.user_id,
+      podcast_id: req.session.podcast_id,
+    });
+
+    const review = reviewData.toJSON();
+
+    res.json(review);
+  } catch (err) {
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+});
+
 // POST /api/reviews -- create one review
 router.post("/", withAuthJson, async (req, res) => {
   try {
@@ -20,8 +39,65 @@ router.post("/", withAuthJson, async (req, res) => {
   }
 });
 
+// GET /api/reviews/ -- get all reviews
+router.get("/", withAuthJson, async (req, res) => {
+  try {
+    const reviewData = await Review.findAll({
+      include: [{ model: Podcast }],
+    });
+    const review = reviewData.toJSON();
+
+    res.json(review);
+  } catch (err) {
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+});
 // PUT /api/reviews/:id -- update one review
 
+router.put("/:id", withAuthJson, async (req, res) => {
+  try {
+    const reviewData = await Review.update(
+      {
+        rating: req.body.rating,
+        review: req.body.review,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(reviewData);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+});
+
 // DELETE /api/reviews/:id -- delete one review
+router.get("/", withAuthJson, async (req, res) => {
+  try {
+    const reviewDelete = await Review.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!reviewDelete) {
+      res.status(404).json({ message: "No review exists for this id" });
+      return;
+    }
+
+    res.status(200).json(reviewDelete);
+  } catch (err) {
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+});
 
 module.exports = router;
